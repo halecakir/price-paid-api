55bee0045092bc4ffc043b35dcc77627571988a0
huseyin.alecakir <huseyin.alecakir@bites.com.tr>
2021-04-04 15:35:56 +0300

    Change json schema path

--- api/config/urls.py
+++ api/config/urls.py
@@ -9,7 +9,7 @@ from drf_spectacular.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("pricepaid.urls")),
-    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/schema/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
